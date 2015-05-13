195ca4ddba58ac74d26dbb3f1e05d81692ccc160
Bart van Merrienboer <bart.vanmerrienboer@gmail.com>
2015-05-13 12:04:46 -0400

    Train LSI model

--- logos.py
+++ logos.py
@@ -1,9 +1,14 @@
-from lxml import etree
-from gensim import corpora
+import logging
 import os
+from lxml import etree
+from gensim import corpora, models
+from nltk import tokenize
+
+logging.basicConfig(level='DEBUG')
+logger = logging.getLogger(__name__)
 
 
-def arxiv_abstracts(path):
+class ArxivAbstracts(corpora.textcorpus.TextCorpus):
     """Generator over abstracts.
 
     Abstracts are lowercased and split on whitespace.
@@ -28,15 +33,24 @@ def arxiv_abstracts(path):
        filtered.
 
     """
-    for article_file in os.listdir(path):
-        with open(article_file) as article:
-            tree = etree.parse(article)
-            # ElementTree needs to namespace passed explicitly
-            description_element = tree.find(
-                './/dc:description',
-                {'dc': 'http://purl.org/dc/elements/1.1/'}
-            )
-            yield description_element.text.lower().split()
+    def __init__(self, path):
+        self.path = path
+        super(ArxivAbstracts, self).__init__(input=True)
 
+    def get_texts(self):
+        for article_file in os.listdir(self.path):
+            with open(os.path.join(self.path, article_file)) as article:
+                tree = etree.parse(article)
+                # ElementTree needs to namespace passed explicitly
+                description_element = tree.find(
+                    './/dc:description',
+                    {'dc': 'http://purl.org/dc/elements/1.1/'}
+                )
+                yield tokenize.word_tokenize(description_element.text.lower())
 
-dictionary = corpora.Dictionary(arxiv_abstracts('../harvest/data/'))
+    def __len__(self):
+        return len(os.listdir(self.path))
+
+corpus = ArxivAbstracts('../harvest/data/')
+corpus.dictionary.filter_extremes()
+lsi = models.lsimodel.LsiModel(corpus, id2word=corpus.dictionary)
